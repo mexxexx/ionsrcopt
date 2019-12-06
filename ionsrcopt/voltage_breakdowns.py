c6cfce2deafb0fe38e4d79fee18e4b3481056008
Max Mihailescu <max.mihailescu@cern.ch>
2019-12-06 10:42:34 +0100

    Voltage breakdowns by variance insted std

--- ionsrcopt/voltage_breakdowns.py
+++ ionsrcopt/voltage_breakdowns.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def classify_using_std_threshold(values, threshold):
-    """ Classify values based on the standard deviation exceding a certain threshold """
+def classify_using_var_threshold(values, threshold):
+    """ Classify values based on the variance exceeding a certain threshold """
 
-    std = np.std(values)
-    return int(std >= threshold)
+    var = np.var(values)
+    return int(var >= threshold)
 
 def detect_breakdowns(df, column='IP.NSRCGEN:SOURCEHTAQNI', window_size=40, threshold=0.5):
     """ Detection of high voltage breakdown based on standard deviation exceding a certain threshold that has to be determined by experiments.
@@ -13,7 +13,7 @@ def detect_breakdowns(df, column='IP.NSRCGEN:SOURCEHTAQNI', window_size=40, thre
     Parameters:
         df (DataFrame): The frame containing the data
         column (string): High voltage current, typically this should be 'IP.NSRCGEN:SOURCEHTAQNI' 
-        window_size (int): Size of the rolling window. Once a breakdown is detected, every value in this window will be set to the integer timestamp of the first point inside the breakdown.
+        window_size (int): Size of the rolling window. Once a breakdown is detected, every value in this window will be set to 1.
         threshold (double): Threshold for the standard deviation.
     
     Returns: 
@@ -23,12 +23,19 @@ def detect_breakdowns(df, column='IP.NSRCGEN:SOURCEHTAQNI', window_size=40, thre
     if not column in df:
         raise ValueError("Error: The column cannot be found in the dataframe.")
 
+    min_index = df.iloc[0].name
+
     result = np.zeros(len(df.index))
     values = df[column].values
-
+    times = (df.index.astype('int64') * 1E-9).values
+    
+    current_breakdown = 0
     for i in range(len(values) - window_size):
-        is_breakdown = classify_using_std_threshold(values[i:i+window_size], threshold) #TODO: Change to time weighted windows
+        is_breakdown = classify_using_var_threshold(values[i:i+window_size], threshold)
         if is_breakdown:
-            result[i:(i + window_size)] = 1
+            if not result[i]:
+                current_breakdown = times[i]
+                
+            result[i:(i + window_size)] = current_breakdown
 
     return result
